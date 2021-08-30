a131e765b4d8e2869c2390cd7d6972e8d44dbed1
Abdullah ahmed <78423536+AbdullahAhmed11@users.noreply.github.com>
2021-08-31 00:21:47 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,68 @@
+//variable 
+
+const closeBtn = document.querySelector(".close");
+const containerMediaMobile = document.querySelector(".container-media-mobile");
+const icon = document.querySelector(".close i");
+const links = document.querySelectorAll(".links li a");
+const solutions = document.querySelector(".solutions");
+const number = document.querySelectorAll(".counter .number");
+const scrollTopBtn = document.querySelector(".scrollup");
+
+
+
+//nav toggle
+closeBtn.addEventListener("click", function() {
+    containerMediaMobile.classList.toggle("show");
+    icon.classList.toggle("fa-window-close");
+})
+
+
+//window scroll counter 
+window.addEventListener("scroll", function() {
+    let solutionOffest = solutions.offsetTop;
+    let winScroll  = this.scrollY;
+    if (winScroll >= solutionOffest - 150) {
+        // init counter 
+        number.forEach((item) => {
+            let i = 0;
+            let countDown = this.setInterval(function () {
+                i++;
+                item.textContent = i;
+                if (i == item.getAttribute("data-number")) {
+                    clearInterval(countDown);
+                }
+            },10)
+        })
+    }
+})
+// Show Scroll Up Btn
+window.onscroll = () => {
+    if(this.scrollY > 300) {
+        scrollTopBtn.style.display = "block"
+    }else {
+        scrollTopBtn.style.display = "none"
+    }
+}
+scrollTopBtn.onclick = () => {
+    window.scrollTo(0, 0)
+
+}
+
+
+// Syinc Links With Section
+links.forEach(link => {
+    link.addEventListener("click", function(e){
+      e.preventDefault();
+      let attrLink = link.getAttribute("data-section");
+      let classSection = `.${attrLink}`;
+      let offsetSection =  document.querySelector(classSection).offsetTop;
+      window.scrollTo(10, offsetSection);
+  
+      // Handle Class Active
+      links.forEach(item => {
+        item.parentElement.classList.remove("active")
+      })
+      link.parentElement.classList.add("active")
+    })
+  })
+
